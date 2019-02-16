31fb45434fea3665667dd4730043731d958be3a0
Douglas Waltman II <doug@dougwaltman.com>
2019-02-16 14:44:01 -0800

    feat(repo): fixes semanitic release config

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,56 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: [
-    'react-hot-loader/patch',
-    path.join(__dirname, '/src/chop-lines.tsx')
+    "react-hot-loader/patch",
+    path.join(__dirname, "/src/chop-lines.tsx")
   ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'chop-lines.js',
-    library: 'chop-lines',
-    libraryTarget: 'umd',
-    publicPath: 'dist/',
-    umdNamedDefine: true,
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: "chop-lines",
+    libraryTarget: "umd",
+    publicPath: "dist/",
+    umdNamedDefine: true
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
     alias: {
-      react: path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-    },
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom")
+    }
   },
   externals: {
     react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'React',
-      root: 'React',
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
     },
     "react-dom": {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'ReactDOM',
-      root: 'ReactDOM',
-    },
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
   },
-  mode: 'production',
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.[jt]sx?$/,
         exclude: /node_modules/,
-        loaders: ['babel-loader'],
+        loaders: ["babel-loader"]
       },
       {
         test: /\.[jt]sx?$/,
-        loader: 'babel-loader',
+        loader: "babel-loader"
       },
       {
         test: /\.[jt]sx?$/,
-        use: ['source-map-loader'],
-        enforce: 'pre',
-      },
-    ],
-  },
+        use: ["source-map-loader"],
+        enforce: "pre"
+      }
+    ]
+  }
 };
